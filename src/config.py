38edc402566dd2d4b205b6b4838a780c890ea09c
mat sob <m.sobczak.05@interia.pl>
2025-06-11 00:43:16 +0200

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+REGION_COORDINATES: Dict[str, tuple] = {
+    "Tatry": (49.2320, 20.0000),
+    "Beskidy": (49.6000, 19.0000),
+    "Pieniny": (49.4200, 20.4100),
+    "Bieszczady": (49.1500, 22.5000)
+}
+
+
+CACHE_DIR = "cache" 
